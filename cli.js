eec07b243ebdfeda73f3d3632cb63c69314abb41
Stefan Buck <s.buck@mwaysolutions.com>
2014-10-30 14:49:54 +0100

    clean up menu file

--- cli.js
+++ cli.js
@@ -6,7 +6,7 @@ var path = require('path');
 var _ = require('lodash');
 var nopt = require('nopt');
 var eventBus = require('./lib/util/eventbus.js');
-var commands = require('./lib/commands');
+// var commands = require('./lib/commands');
 
 var options = {
   debug: true
@@ -14,37 +14,33 @@ var options = {
 
 var opts = nopt();
 var args = opts.argv.remain;
-var cmd = args.shift();
+var cmd = args[0];
+
+// cli is called with a short hand like `mcap new`
+var data = {
+  cmd: cmd,
+  args: args,
+  opts: _.omit(opts, 'argv')
+};
 
 // Setup the commands and register the listiners
-commands();
+//commands();
 
-if (!cmd) {
+// Register the `home generator.
+var env = require('yeoman-environment').createEnv();
+env.on('end', function () {
+  console.log('Done running sir');
+});
 
-  // Register the `home generator.
-  var env = require('yeoman-environment').createEnv();
-  env.on('end', function () {
-    console.log('Done running sir');
-  });
+env.on('error', function (err) {
+  console.error('Error', process.argv.slice(2).join(' '), '\n');
+  console.error(options.debug ? err.stack : err.message);
+});
 
-  env.on('error', function (err) {
-    console.error('Error', process.argv.slice(2).join(' '), '\n');
-    console.error(options.debug ? err.stack : err.message);
-  });
+env.lookup(function() {
+  env.register(path.resolve(__dirname, './lib/menu'), 'menu');
 
-  env.lookup(function() {
-    env.register(path.resolve(__dirname, './lib/menu'), 'menu');
-    env.run(['menu'], {});
-  });
-
-} else {
-
-  // cli is called with a short hand like `mcap new`
-  var data = {
-    cmd: cmd,
-    args: opts.argv.remain,
-    opts: _.omit(opts, 'argv')
-  };
-
+  args.unshift('menu');
+  env.run(args, opts);
   eventBus.emit(eventBus.CMD_EVENT, data);
-}
+});
